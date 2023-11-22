a1031e8c6a214ea27783e7fd056afc6a299b950b
Marco Ribeiro <marco@statix.com.br>
2023-11-22 09:38:52 -0300

    test: trying useMemo (optimize calcs in variables)

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,9 +1,11 @@
-import { useEffect, useState } from "react";
+import { useMemo, useEffect, useState } from "react";
 
 export const Login = () => {
+    // HOOK: useState
     const [password, setPassword] = useState('');
     const [email, setEmail] = useState('');
 
+    // HOOK: useEffect
     // Executado somente quando o componente for executado pela primeira vez (e não toda vez que é renderizado), caso o array de dependências seja vazio!
     // UseEffect é perfeito para chamadas de APIs, por exemplo
     useEffect(() => {
@@ -28,13 +30,25 @@ export const Login = () => {
     //    chamadaDeApi(page)
     // }, [page]);
 
+    // HOOK: useMemo
+    // Utilizado para realizar cálculos mais complexos e deixar armazenado na memória (armazenado em uma variável)
+    // para que o cálculo não seja realizado várias vezes 
+    // Array de Dependências (sempre que algum elemento dele for alterado, função do useMemo será executada)
+    // Sempre deve-se informar no array de dependências, a variável que está sendo usada na função
+    const emailLength = useMemo(() => {
+        return email.length * 1000;
+    }, [email.length]);
+
+
     const handleEntrar = () => {
 
     }
 
+
     return (
         <div>
             <form>
+                <p>Quantidade de caracteres no email: {emailLength}</p>
 
                 <label>
                     <span>Email</span>
