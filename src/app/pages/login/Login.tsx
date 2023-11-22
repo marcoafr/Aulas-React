5d1b4a0ffd7f94f8e14a28b949a472ec7c4c944d
Marco Ribeiro <marco@statix.com.br>
2023-11-22 09:07:43 -0300

    feat: add login page and form

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,16 +1,33 @@
-import {useNavigate} from "react-router-dom"
+import { useState } from "react";
 
 export const Login = () => {
-    const history = useNavigate();
-    
-    const handleClick = () => {
-        history('pagina-inicial');
+    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+
+    const handleEntrar = () => {
+        console.log(email);
+        console.log(password);
     }
 
     return (
         <div>
-            Login
-            <button onClick={handleClick}>Página Inicial</button>
+            <form>
+
+                <label>
+                    <span>Email</span>
+                    <input value={email} onChange={e => setEmail(e.target.value)} />
+                </label>
+
+                <label>
+                    <span>Senha</span>
+                    <input type="password" value={password} onChange={e => setPassword(e.target.value)}/>
+                </label>
+
+                <button type="button" onClick={handleEntrar}>
+                    Entrar
+                </button>
+
+            </form>
         </div>
     )
 }
